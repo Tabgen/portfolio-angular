8d1dbdbff2cde5a181c5252489e6ec1b8e2d3801
Aslak <99125865+Tabgen@users.noreply.github.com>
2023-01-21 00:01:22 +0100

    les README for instruksjoner

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  dark() {
+    document.getElementById("header").style.backgroundColor = "#232323";
+    document.getElementById("about-container").style.backgroundColor = "#232323";
+    document.getElementById("projects").style.backgroundColor = "#232323";
+    document.getElementById("modal").style.backgroundColor = "#232323";
+    document.getElementById("close").style.backgroundColor = "#232323";
+    document.getElementById("cname").style.backgroundColor = "#232323";
+    document.getElementById("cname").style.borderColor = "white";
+    document.getElementById("cname").style.color = "white";
+    document.getElementById("cpost").style.backgroundColor = "#232323";
+    document.getElementById("cpost").style.borderColor = "white";
+    document.getElementById("cpost").style.color = "white";
+    document.getElementById("cmessage").style.backgroundColor = "#232323";
+    document.getElementById("cmessage").style.borderColor = "white";
+    document.getElementById("cmessage").style.color = "white";
+    document.getElementById("contact").style.backgroundColor = "#232323";
+    document.getElementById("close").style.color = "white";
+    document.getElementById("Hello").style.color = "white";
+    document.getElementById("about").style.color = "white";
+    document.getElementById("projects").style.color = "white";
+    document.getElementById("web").style.color = "white";
+    document.getElementById("git").style.color = "white";
+    document.getElementById("headline").style.color = "white";
+    document.getElementById("readmore").style.color = "#4361ee";
+    document.getElementById("submit").style.color = "black";
+    document.getElementById("submit").style.backgroundColor = "white";
+  }
+
 }
