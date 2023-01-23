7dc49b61d480d8115664dac1839d0e3af1f89813
Aslak <aslaktangen@gmail.com>
2023-01-23 14:34:43 +0100

    mhmhmhmh

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
   dark() {
+    document.getElementById("signaturew").style.display = "block";
+    document.getElementById("signature").style.display = "none";
+    document.getElementById("header").style.color = "white";
+    document.getElementById("lswitch").style.display = "block";
+    document.getElementById("tswtich").style.display = "none";
     document.getElementById("header").style.backgroundColor = "#232323";
     document.getElementById("about-container").style.backgroundColor = "#232323";
     document.getElementById("projects").style.backgroundColor = "#232323";
@@ -33,6 +38,46 @@ export class HeaderComponent {
     document.getElementById("readmore").style.color = "#4361ee";
     document.getElementById("submit").style.color = "black";
     document.getElementById("submit").style.backgroundColor = "white";
+    document.getElementById("login-form").style.backgroundColor = "#232323";
+    document.getElementById("login-form").style.color = "white";
+    
+
+  }
+
+  light() {
+    document.getElementById("signaturew").style.display = "none";
+    document.getElementById("signature").style.display = "block";
+    document.getElementById("header").style.color = "black";
+    document.getElementById("lswitch").style.display = "none";
+    document.getElementById("tswtich").style.display = "block";
+    document.getElementById("header").style.backgroundColor = "white";
+    document.getElementById("about-container").style.backgroundColor = "white";
+    document.getElementById("projects").style.backgroundColor = "white";
+    document.getElementById("modal").style.backgroundColor = "white";
+    document.getElementById("close").style.backgroundColor = "white";
+    document.getElementById("cname").style.backgroundColor = "white";
+    document.getElementById("cname").style.borderColor = "rgba(0, 0, 0, 0.3764705882)";
+    document.getElementById("cname").style.color = "rgba(0, 0, 0, 0.8156862745)";
+    document.getElementById("cpost").style.backgroundColor = "white";
+    document.getElementById("cpost").style.borderColor = "rgba(0, 0, 0, 0.3764705882)";
+    document.getElementById("cpost").style.color = "rgba(0, 0, 0, 0.8156862745)";
+    document.getElementById("cmessage").style.backgroundColor = "white";
+    document.getElementById("cmessage").style.borderColor = "rgba(0, 0, 0, 0.3764705882)";
+    document.getElementById("cmessage").style.color = "rgba(0, 0, 0, 0.8156862745)";
+    document.getElementById("contact").style.backgroundColor = "white";
+    document.getElementById("close").style.color = "black";
+    document.getElementById("Hello").style.color = "black";
+    document.getElementById("about").style.color = "black";
+    document.getElementById("projects").style.color = "black";
+    document.getElementById("web").style.color = "black";
+    document.getElementById("git").style.color = "black";
+    document.getElementById("headline").style.color = "black";
+    document.getElementById("readmore").style.color = "#4361ee";
+    document.getElementById("submit").style.color = "white";
+    document.getElementById("submit").style.backgroundColor = "black";
+    document.getElementById("login-form").style.backgroundColor = "white";
+    document.getElementById("login-form").style.color = "black";
+
   }
 
 }
