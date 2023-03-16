9b7c2e6e17c86969ae3d5137575783156fb089dc
Aslak <99125865+Tabgen@users.noreply.github.com>
2023-03-16 14:32:50 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'tangen-portfolio',
-    appId: '1:508866405014:web:be8ab10d027d45413dd8b7',
-    databaseURL: 'https://tangen-portfolio-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'tangen-portfolio.appspot.com',
-    apiKey: 'AIzaSyBWX367YSYB9ObI0EQ7dCvzPSB49kvhye0',
-    authDomain: 'tangen-portfolio.firebaseapp.com',
-    messagingSenderId: '508866405014',
+    projectId: 'portfolio',
+    appId: '',
+    databaseURL: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },};
